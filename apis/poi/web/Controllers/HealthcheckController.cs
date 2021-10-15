f12d894f3a0076c8bfa2bed288e7af9c46d210ba
rob-foulkrod <89028299+rob-foulkrod@users.noreply.github.com>
2021-10-15 17:27:26 -0400

    tiny bit of injection (#38)
    * tiny bit of injection
    
    * Update codeql-analysis-cs.yml
    
    * Update codeql-analysis-go.yml
    
    * Update codeql-analysis-java.yml
    
    * Update codeql-analysis-js.yml
    
    * writing passwords
    
    * Create codeql-config.yml
    
    * adding connection info
    
    * Update codeql-analysis-cs.yml
    
    * back to one
    
    * really bad

--- apis/poi/web/Controllers/HealthcheckController.cs
+++ apis/poi/web/Controllers/HealthcheckController.cs
@@ -30,6 +30,19 @@ namespace poi.Controllers
             _logger.LogInformation(LoggingEvents.Healthcheck, "Healthcheck Requested");
             return Ok(new Healthcheck());
         }
+
+        [HttpPost]
+        [Produces("application/json", Type = typeof(Healthcheck))]
+        public IActionResult Post()
+        {
+            var cn = new System.Data.SqlClient.SqlConnection("Server=(local);UserId=sa;Password=PA$$W0rd");
+            var sql = "SELECT CompanyName from customers where CustomerID = '" + Request.Form["customerId"] + "'";
+            var cmd = new System.Data.SqlClient.SqlCommand(sql, cn);
+
+            var name = cmd.ExecuteScalar();
+            return Ok(name);
+        }
+
     }
 
 }
